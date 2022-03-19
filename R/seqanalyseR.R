88c2432a3630c8f5eb27abcf50b8c022013444ec
Jiahao-Kuang <101912558+Jiahao-Kuang@users.noreply.github.com>
2022-03-19 17:54:19 +0800

    Add files via upload

--- R/seqanalyseR.R
+++ R/seqanalyseR.R
@@ -3,9 +3,16 @@
 #' A package to provide RNA-seq downstream analysis
 #'
 #' @section Functions :
-#' RNA-seq 下游分析
-#' 用help函数查看下列每一个函数的帮助
-#' @section 1.countTPM :算TPM值，输入RAWDATA
+#' 这个R包共分为八个函数，涵盖归一、差异分析、可视化、富集分析,下面是函数的案例
+#' nyq2022<-read.table（file = "nyq2022.txt",header = T,row.names = 1
+#' group<-read.csv(file = "group.csv",header = T)
+#' @section 1.转换ID
+#' aa<-IDchange( geneIDtoCHANGE = row.names(nyq2022),
+#' CHANGEfromTYPE = "ENSEMBL", CHANGEtoTYPE = "ENTREZID",
+#' SPECIES = "RAT",
+#' omitNA = TRUE,
+#' MERGEtoDATA = nyq2022,
+#' drop = TRUE )
 #' @section 2.differenceALALYSIS :调用DESeq做差异分析
 #' @section 做富集分析之前要转换ID成ENTRIZID
 #' @section 3.IDchange :可以改变gene ID的类型，可以给列表，也可以直接把RAWDATA的gene ID列放进来，如果有需要可以设置merge到rawdata和去除未匹配的值
@@ -15,8 +22,8 @@
 #' @section 6.plotVOCANOL :画火山图
 #' @section 7.plotHEATMAP1 :画热图，比较符合审美
 #' @section 8.plotHEATMAP2 :画热图，更加符合审美
-#' @section 9.DOWNstreamANALYSIS 禁忌的函数，一点不对都会报错，测试过程中画出的图都很诡异
-#' 但是直接用它可以走完整个下游流程，不需要指定输出，运气好可以直接看到图片在工作目录生成
+#' @section 9.DOWNstreamANALYSIS : 走完整个下游分析
+#'
 #'
 #' @encoding UTF-8
 #' @docType pacakge
